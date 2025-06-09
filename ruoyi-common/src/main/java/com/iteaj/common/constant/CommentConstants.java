c7729f1da45e58ae3b77683567a3e885469c3411
qiancheng1 <qiancheng1@noreply.gitcode.com>
2025-06-09 18:49:44 +0800

    合并1

---
+++ ruoyi-common/src/main/java/com/iteaj/common/constant/CommentConstants.java
@@ -0,0 +1,21 @@
+package com.iteaj.common.constant;
+
+/**
+ * 公共常量
+ *
+ * @author qiancheng
+ * @date 2025-03-12
+ */
+public class CommentConstants
+{
+    /**
+     * 是
+     */
+    public static final String PUBLIC_YES = "1";
+
+    /**
+     * 否
+     */
+    public static final String PUBLIC_NO = "0";
+
+}
